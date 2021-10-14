7665e5711c1f3dceb1c64e5bf17a86b004748611
Anjali Singh <90452967+AnjaliSingh2002@users.noreply.github.com>
2021-10-14 11:46:03 +0530

    Update mango.java add variable weight (group-A)

--- Mango.java
+++ Mango.java
@@ -1,8 +1,23 @@
-package Assignment_Fruit;
+
+   package Assignment_Fruit;
 
 public class Mango {
+private double weight;
 
-    public String toString() {
+      public String toString() {
         return "This is Mango created by Anjali";
     }
+
+
+// Getter
+
+    public double getWeight() {
+        return weight;
+    }
+
+    //Setter
+    public void setWeight(double newWeight) {
+        this.weight = newWeight;
+    }
 }
+
