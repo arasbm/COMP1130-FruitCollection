aae4b4024f9b83a5b54d84e01094451159313dac
Vinit Jangir <90442953+vinitjangir18@users.noreply.github.com>
2021-10-16 15:09:36 +0530

    Group B - Final Submission [GroupB_FruitSalad.java]
    Every member in my group did their part, we had several group discussion regarding the assignment and we helped each other along the way.

---
+++ GroupB_FruitSalad.java
@@ -0,0 +1,98 @@
+/*
+    Project: A Java project created for Getter and Setter methods.
+    Group B.
+    Group Members: Anthony, Jay, Ujjwal, Vinit.
+    Date: October 16, 2021.
+*/
+
+import java.util.Scanner;
+
+
+public class GroupB_FruitSalad {
+    public static void main(String[] args) {
+        Cherry cherry = new Cherry();
+        PineApple pineapple = new PineApple();
+        DragonFruit dragonfruit = new DragonFruit();
+        Apple apple = new Apple();
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("\nWelcome to Team B's Fruit Salad Maker!\n");
+        System.out.println("Please enter how many grams of Cherry your want in your salad: ");
+        cherry.setGrams(scan.nextDouble());
+        System.out.println("Please enter how many grams of PineApple your want in your salad: ");
+        pineapple.setWeight(scan.nextDouble());
+        System.out.println("Please enter how many grams of DragonFruit your want in your salad: ");
+        dragonfruit.setWeight(scan.nextDouble());
+        System.out.println("Please enter how many grams of Apple your want in your salad: ");
+        apple.setWeight(scan.nextDouble());
+
+        double totalgrams = cherry.getGrams() + pineapple.getWeight() + dragonfruit.getWeight() + apple.getWeight();
+        String ingredients = "";
+
+        /* Nested if else statements. */
+       if(pineapple.getWeight() > 0 && cherry.getGrams() > 0 && dragonfruit.getWeight() > 0 && apple.getWeight() == 0){                         
+            ingredients = "Cherry, PineApple, DragonFruit.\n";
+        }
+
+        else if(pineapple.getWeight() > 0 && cherry.getGrams() > 0 && dragonfruit.getWeight() == 0 && apple.getWeight() > 0){                         
+            ingredients = "Cherry, PineApple, Apple.\n";
+        }
+
+        else if(pineapple.getWeight() > 0 && cherry.getGrams() == 0 && dragonfruit.getWeight() > 0 && apple.getWeight() > 0){                         
+            ingredients = "PineApple, DragonFruit, Apple.\n";
+        }
+
+        else if(pineapple.getWeight() == 0 && cherry.getGrams() > 0 && dragonfruit.getWeight() > 0 && apple.getWeight() > 0){                         
+            ingredients = "Cherry, DragonFruit, Apple.\n";
+        }
+
+        else if(pineapple.getWeight() > 0 && cherry.getGrams() > 0 && dragonfruit.getWeight() == 0 && apple.getWeight() == 0){                         
+            ingredients = "Cherry, PineApple.\n";
+        }
+
+        else if(pineapple.getWeight() > 0 && cherry.getGrams() == 0 && dragonfruit.getWeight() > 0 && apple.getWeight() == 0){                         
+            ingredients = "PineApple, DragonFruit.\n";
+        }
+
+        else if(pineapple.getWeight() == 0 && cherry.getGrams() > 0 && dragonfruit.getWeight() == 0 && apple.getWeight() > 0){                         
+            ingredients = "Cherry, Apple.\n";
+        }
+
+        else if(pineapple.getWeight() > 0 && cherry.getGrams() == 0 && dragonfruit.getWeight() == 0 && apple.getWeight() > 0){                         
+            ingredients = "PineApple, Apple.\n";
+        }
+
+        else if(pineapple.getWeight() == 0 && cherry.getGrams() > 0 && dragonfruit.getWeight() > 0 && apple.getWeight() == 0){                         
+            ingredients = "Cherry, DragonFruit.\n";
+        }
+
+        else if(pineapple.getWeight() == 0 && cherry.getGrams() == 0 && dragonfruit.getWeight() > 0 && apple.getWeight() > 0){                         
+            ingredients = "DragonFruit, Apple.\n";
+        }
+
+        else if(pineapple.getWeight() > 0){                         
+            if(cherry.getGrams() > 0){
+                if(dragonfruit.getWeight() > 0){
+                    if(apple.getWeight() > 0){
+                        ingredients = "Cherry, PineApple, DragonFruit, Apple.\n";
+                    }
+                }
+            }
+        }
+
+        else if(pineapple.getWeight() <= 0){                                                                   
+            if(cherry.getGrams() <= 0){
+                if(dragonfruit.getWeight() <= 0){
+                    if(apple.getWeight() <= 0){
+                        ingredients = "Nothing Selected :(\n";
+                    }
+                }
+            }
+        }
+
+        System.out.printf("\n\nThank you for your order! Your fruit salad is ready.");
+        System.out.printf("\nTotal weight of your salad is %.2fg. \nIngredients: %s", totalgrams, ingredients);
+        scan.close();
+    }
+}
