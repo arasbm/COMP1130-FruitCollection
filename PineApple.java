1b91d43c74629cdf1d969951966c40bac2f1335c
Jay Mistry <90444018+Cipher-935@users.noreply.github.com>
2021-10-16 10:29:34 +0530

    Included Getter and Setter

--- PineApple.java
+++ PineApple.java
@@ -1,7 +1,19 @@
 
 public class PineApple {
+     private double weight;//private variable declaration
+	
      public String toString() {
 		return "This is a Pineapple created by Jay";
     	 
      }
+	// Getter method created
+     public double getWeight(){
+		 return weight;
+	 }
+	// Setter method created
+     public void setWeight(double uniqueweight)
+	{
+		this.weight = uniqueweight;
+	}
+	
 }
