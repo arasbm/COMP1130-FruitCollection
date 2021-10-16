5106335f0bfd2e33d6a25901d983c9a908fb6fe0
Vinit Jangir <90442953+vinitjangir18@users.noreply.github.com>
2021-10-16 13:19:11 +0530

    Updated Cherry.java with Getter and Setter methods - Team B

--- Cherry.java
+++ Cherry.java
@@ -1,8 +1,13 @@
 package Assignment_toString;
 
 public class Cherry {
+    private double grams;
+    
     public String toString() {
         String toString = "This is Cherry created by Vinit";
         return  toString;
     }
+    
+    public double getGrams(){ return grams; } // Getter method created.
+    public void setGrams(double received) { this.grams = received; } // Setter method created.
 }
