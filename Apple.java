0c9db3e3030911888bab862622e44d2212b8dffe
AnthonyRugwabiza <90490670+AnthonyRugwabiza@users.noreply.github.com>
2021-10-16 03:29:22 -0700

    Apple.java updated using getter and setter - Team B

--- Apple.java
+++ Apple.java
@@ -1,8 +1,18 @@
 package Assignment_toString;
 
 public class Apple {
+    private double weight; //variable created.
+    
     public String toString() {
         String toString = "Apple made by Anthony";
         return toString;
+    }
+    
+    public double getWeight() { //getter method
+        return weight;
+    }
+    
+    public void setWeight(double variable) { //setter method
+        this.weight = variable;
     }
 }
