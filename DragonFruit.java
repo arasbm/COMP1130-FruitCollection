06d29442a431bc4c7c15d16cf0d2ce929168d0cb
soodujjwal <90448948+soodujjwal@users.noreply.github.com>
2021-10-16 10:38:17 +0530

    updated with getter setter

--- DragonFruit.java
+++ DragonFruit.java
@@ -1,8 +1,20 @@
 package dragonfruit;
 
 public class DragonFruit {
+    
+    private double weight;
+    
     public String toString(){
        String toString = "This is a DragonFruit created by Ujjwal Sood";
        return toString;
     }
+    
+    public double getWeight(){
+        return weight;  //getter method 
+    }
+    
+    public void setWeight(double correctWeight){
+        this.weight = correctWeight;   //setter method
+    }
+            
 }
